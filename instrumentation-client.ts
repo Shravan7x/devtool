0b7eb28d07dc6103da278eca57b3e0067f8436a7
Shravan7x <Shravanp50rj3@gmail.com>
2026-01-13 14:23:33 +0530

    post hog  pacakage intalled and implementation

---
+++ instrumentation-client.ts
@@ -0,0 +1,7 @@
+// instrumentation-client.ts
+import posthog from 'posthog-js'
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? 'https://app.posthog.com',
+  defaults: '2025-11-30',
+})
